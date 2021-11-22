021aa2d8e3e6a51c73ea7df1f10895e45c26933d
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-11-22 03:08:07 -0500

    Create Rails routes and edit root page

--- frontend/disneyCinema.jsx
+++ frontend/disneyCinema.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import * as UserApi from './util/user_api_util';
+import * as SessionApi from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
   ReactDOM.render(<Root store={store} />, root);
+  window.UserApi = UserApi;
+  window.SessionApi = SessionApi;
 });
