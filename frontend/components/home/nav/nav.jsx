da373bee319904b64ac9e0b40f6227d08e1b32dd
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2021-11-25 18:14:07 -0500

    Revert "Add effects to navbar"
    This reverts commit 3e3e7ccad4ca1fe43b474acce37463f70098b98d.

--- frontend/components/home/nav/nav.jsx
+++ frontend/components/home/nav/nav.jsx
@@ -1,52 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import NavLinks from './nav_links'
 
-function Nav() {
-  const [show, handleShow] = useState(false);
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 585) {
-        handleShow(true);
-      }
-      else {
-        handleShow(false);
-      }
-    });
-    return () => {
-      window.removeEventListener('scroll');
-    };
-  }, []);
-
-  return (
-    <div className={`nav-bar ${show && "nav-show"}`} id="navbar">
-      <Link to="/home">
-        <img src={window.logo} className="nav-logo" />
-      </Link>
-      <NavLinks />
-    </div>
-  );
+  render() {
+    return (
+      <div className="nav-bar">
+        <Link to="/home">
+          <img src={window.logo} className="nav-logo" />
+        </Link>
+        <NavLinks />
+      </div>
+    );
+  }
 }
 
 export default Nav;
-
-
-// class Nav extends React.Component {
-//   constructor(props) {
-//     super(props)
-//   }
-
-//   render() {
-//     return (
-//       <div className={`nav-bar ${show && "nav-show"}`} id="navbar">
-//         <Link to="/home">
-//           <img src={window.logo} className="nav-logo" />
-//         </Link>
-//         <NavLinks />
-//       </div>
-//     );
-//   }
-// }
-
-// export default Nav;
