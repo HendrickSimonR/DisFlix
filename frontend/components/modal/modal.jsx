cfb2413d3c9f6c711f5d86d86563ef576192936a
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-01-13 20:39:27 -0500

    Begin implementing modal

---
+++ frontend/components/modal/modal.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { closeModal } from '../../actions/modal_actions';
+import { connect } from 'react-redux';
+
+function Modal({modal, closeModal}) {
+  if (!modal) {
+    return null;
+  }
+  
+  let component;
+  
+  switch (modal) {
+    case 'editProfile':
+      component = <EditProfileModalContainer/>;
+      break;
+    case 'movieInfo':
+      component = <MovieInfoModal/>;
+      break;
+    default:
+      return null;
+  }
+  
+  return (
+    <div className="modal-background" onClick={closeModal}>
+      <div className="modal-child" onClick={e => e.stopPropagation()}>
+        { component }
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    modal: state.modal,
+    props: state.props
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    closeModal: () => dispatch(closeModal())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -5,14 +5,17 @@ import sessionReducer from './session_reducer';
 import likesReducer from './likes_reducer';
 import dislikesReducer from './dislikes_reducer';
 import moviesReducer from './movies_reducer';
+import modalReducer from './modal_reducer';
 import brandsReducer from './brands_reducer';
 import watchlistReducer from './watchlist_reducer';
+import modalReducer from './modal_reducer';
 
 const rootReducer = combineReducers({
   brands: brandsReducer,
   errors: errorsReducer,
   likes: likesReducer,
   dislikes: dislikesReducer,
+  modal: modalReducer,
   movies: moviesReducer,
   session: sessionReducer,
   user: usersReducer,
