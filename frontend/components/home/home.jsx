ca0a7d866da4476025eedef243c36ed40d505fd1
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-01-13 19:10:26 -0500

    Add arrow hide and show functionality for all movie rows

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -38,72 +38,154 @@ class Home extends React.Component {
 
   handleScroll = (direction, brand) => {
     if (direction === 'left') {
+
       if (brand === 'disney') {
         this.disney.current.style.transform = 'translateX(0px)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle disney swiped");
         mid[0].classList.remove('swiped');
-        
-        // let newRule = 'thumbnail-container.middle.disney { transform: scale(1.4) translate(-1.3vw, -48px) !important}'
-        // console.log('disneymid', disneyMid[0]);
-        // disneyMid[0].style.transform = 'scale(1.4) translate(-1.3vw, -48px) !important';
-        // console.log('ATTEMPT', window.getComputedStyle(disneyMid, null))
+
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow disney");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow disney hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       } else if (brand === 'pixar') {
         this.pixar.current.style.transform = 'translateX(0px)';
+
         let mid = document.getElementsByClassName("thumbnail-container middle pixar swiped");
         mid[0].classList.remove('swiped');
-        // document.getElementById("left-arrow").classList.toggle("hide");
+
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow pixar");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow pixar hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       } else if (brand === 'marvel') {
         this.marvel.current.style.transform = 'translateX(0px)';
+
         let mid = document.getElementsByClassName("thumbnail-container middle marvel swiped");
         mid[0].classList.remove('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow marvel");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow marvel hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       } else if (brand === 'starWars') {
         this.starWars.current.style.transform = 'translateX(0px)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle star-wars swiped");
         mid[0].classList.remove('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow star-wars");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow star-wars hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       } else if (brand === 'natGeo') {
         this.natGeo.current.style.transform = 'translateX(0px)';
+
         let mid = document.getElementsByClassName("thumbnail-container middle nat-geo swiped");
         mid[0].classList.remove('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow nat-geo");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow nat-geo hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       } else {
         this.watchlist.current.style.transform = 'translateX(0px)';
+
         let mid = document.getElementsByClassName("thumbnail-container middle watchlist swiped");
         mid[0].classList.remove('swiped');
+
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow watchlist");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow watchlist hidden");
+
+        leftArrow[0].classList.add('hidden');
+        rightArrow[0].classList.remove('hidden');
+
       }
 
     } else if (direction === 'right') {
 
       if (brand === 'disney') {
         this.disney.current.style.transform = 'translateX(-78.5%)';
+
         let mid = document.getElementsByClassName("thumbnail-container middle disney");
         mid[0].classList.add('swiped');
+        
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow disney hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow disney");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
 
       } else if (brand === 'pixar') {
         this.pixar.current.style.transform = 'translateX(-78.5%)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle pixar");
         mid[0].classList.add('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow pixar hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow pixar");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
+
       } else if (brand === 'marvel') {
         this.marvel.current.style.transform = 'translateX(-78.5%)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle marvel");
         mid[0].classList.add('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow marvel hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow marvel");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
+
       } else if (brand === 'starWars') {
         this.starWars.current.style.transform = 'translateX(-78.5%)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle star-wars");
         mid[0].classList.add('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow star-wars hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow star-wars");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
+
       } else if (brand === 'natGeo') {
         this.natGeo.current.style.transform = 'translateX(-78.5%)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle nat-geo");
         mid[0].classList.add('swiped');
 
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow nat-geo hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow nat-geo");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
+
       } else {
         this.watchlist.current.style.transform = 'translateX(-78.5%)';
+        
         let mid = document.getElementsByClassName("thumbnail-container middle watchlist");
         mid[0].classList.add('swiped');
+
+        let leftArrow = document.getElementsByClassName("material-icons left-arrow watchlist hidden");
+        let rightArrow = document.getElementsByClassName("material-icons right-arrow watchlist");
+
+        leftArrow[0].classList.remove('hidden');
+        rightArrow[0].classList.add('hidden');
       }
     }
   }
@@ -240,10 +322,10 @@ class Home extends React.Component {
                 <h1 className="row-header">Watchlist</h1>
 
                 <div className="scroll-arrows">
-                  <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'watchlist')}>
+                  <span className="material-icons left-arrow watchlist hidden" onClick={() => this.handleScroll('left', 'watchlist')}>
                     arrow_back_ios
                   </span>
-                  <span className="material-icons" id="right-arrow" onClick={() => this.handleScroll('right', 'watchlist')}>
+                  <span className="material-icons right-arrow watchlist" onClick={() => this.handleScroll('right', 'watchlist')}>
                     arrow_forward_ios
                   </span>
                 </div>
@@ -269,10 +351,10 @@ class Home extends React.Component {
               <h1 className="row-header">Disney</h1>
                 
               <div className="scroll-arrows">
-                <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'disney')}>
+                <span className="material-icons left-arrow disney hidden" onClick={() => this.handleScroll('left', 'disney')}>
                   arrow_back_ios
                 </span>
-                <span className="material-icons" id="right-arrow"  onClick={() => this.handleScroll('right', 'disney')}>
+                <span className="material-icons right-arrow disney"  onClick={() => this.handleScroll('right', 'disney')}>
                   arrow_forward_ios
                 </span>
               </div>
@@ -289,10 +371,10 @@ class Home extends React.Component {
               <h1 className="row-header">Pixar</h1>
                 
               <div className="scroll-arrows">
-                <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'pixar')}>
+                <span className="material-icons left-arrow pixar hidden" onClick={() => this.handleScroll('left', 'pixar')}>
                   arrow_back_ios
                 </span>
-                <span className="material-icons" id="right-arrow"  onClick={() => this.handleScroll('right', 'pixar')}>
+                <span className="material-icons right-arrow pixar" onClick={() => this.handleScroll('right', 'pixar')}>
                   arrow_forward_ios
                 </span>
               </div>
@@ -308,10 +390,10 @@ class Home extends React.Component {
               <h1 className="row-header">Marvel</h1>
 
               <div className="scroll-arrows">
-                <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'marvel')}>
+                <span className="material-icons left-arrow marvel hidden" onClick={() => this.handleScroll('left', 'marvel')}>
                   arrow_back_ios
                 </span>
-                <span className="material-icons" id="right-arrow"  onClick={() => this.handleScroll('right', 'marvel')}>
+                <span className="material-icons right-arrow marvel" onClick={() => this.handleScroll('right', 'marvel')}>
                   arrow_forward_ios
                 </span>
               </div>
@@ -327,10 +409,10 @@ class Home extends React.Component {
               <h1 className="row-header">Star Wars</h1>
 
               <div className="scroll-arrows">
-                <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'starWars')}>
+                <span className="material-icons left-arrow star-wars hidden" onClick={() => this.handleScroll('left', 'starWars')}>
                   arrow_back_ios
                 </span>
-                <span className="material-icons" id="right-arrow" onClick={() => this.handleScroll('right', 'starWars')}>
+                <span className="material-icons right-arrow star-wars" onClick={() => this.handleScroll('right', 'starWars')}>
                   arrow_forward_ios
                 </span>
               </div>
@@ -346,10 +428,10 @@ class Home extends React.Component {
               <h1 className="row-header ">National Geographic</h1>
                 
               <div className="scroll-arrows">
-                <span className="material-icons" id="left-arrow" onClick={() => this.handleScroll('left', 'natGeo')}>
+                <span className="material-icons left-arrow nat-geo hidden" onClick={() => this.handleScroll('left', 'natGeo')}>
                   arrow_back_ios
                 </span>
-                <span className="material-icons" id="right-arrow" onClick={() => this.handleScroll('right', 'natGeo')}>
+                <span className="material-icons right-arrow nat-geo" onClick={() => this.handleScroll('right', 'natGeo')}>
                   arrow_forward_ios
                 </span>
               </div>
