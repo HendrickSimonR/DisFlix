104ceb61bbd4c952f4dbe950462147ebd7278bd4
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-01-19 14:28:10 -0500

    Fix Heroku seeds

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,12 +5,10 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
 const usersReducer = ( oldState = {}, action ) => {
-  Object.freeze(oldState);
   const newState = Object.assign({}, oldState);
 
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      // newState[action.user.id] = action.user;
       return action.user;
     default:
       return oldState;
