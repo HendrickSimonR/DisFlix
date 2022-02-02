79d40d1a8617a774a899f1029d5999abb8c3b956
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-01 21:04:22 -0500

    Test for Heroku

--- app/views/api/brands/index.json.jbuilder
+++ app/views/api/brands/index.json.jbuilder
@@ -1,6 +1,6 @@
 json.array! @brands do |brand|
   json.extract! brand, :id, :name
   json.image_url url_for(brand.image)
-  # json.movie_url url_for(brand.movie)
+  json.movie_url url_for(brand.movie)
   # json.button_video_url url_for(brand.button_video)
 end
--- frontend/components/thumbnail/thumbnail_container.jsx
+++ frontend/components/thumbnail/thumbnail_container.jsx
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router';
 import Thumbnail from './thumbnail';
 
 const mSTP = state => {
-  // console.log('THUMBSTATE', state);
+  console.log('THUMBSTATE', state);
   return {
     test: state.brands[state.brands.length - 1].movie_url,
     user: state.session.id,
