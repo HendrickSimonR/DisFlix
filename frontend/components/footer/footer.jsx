62c764466292e390716a3d91f5ee2fd2a468a426
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-04 23:44:24 -0500

    Edit footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <div className={!window.location.href.includes('home') ? "footer brand" : "footer"}>
-        <h1 className="footer-details">Thank you for stopping by!
-        This website is not-for-profit.
-        I simply love all things Disney!
-        Please contact me at my Github for any queries.</h1>
+        <h1 className="footer-details">
+          Thank you for stopping by!
+          This website is not-for-profit.
+          I simply love all things Disney!
+          Please contact me using the links in the dropdown menu.
+        </h1>
     </div>
   )
 }
