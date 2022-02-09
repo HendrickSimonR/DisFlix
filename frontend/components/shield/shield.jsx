0d78caf1a176eedbabda8542e14dc40faab7f21c
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-08 21:05:52 -0500

    Add phone screen

--- frontend/components/shield/shield.jsx
+++ frontend/components/shield/shield.jsx
@@ -4,7 +4,12 @@ class Shield extends React.Component {
   render() {
     return(
       <div className='shield'>
-        <h1>Please turn device to continue</h1>        
+        <h1>Please turn device to continue</h1>   
+        <img 
+          alt='' 
+          className='shield-gif'
+          src="https://media4.giphy.com/media/I6EJeAK6a82udA6Snc/giphy.gif?cid=790b7611f53f995d9e7e9232144d733c7d911f378edc2afc&rid=giphy.gif&ct=s" 
+        />     
       </div>
     )
   }
