b6b979f98f9eaf6d1ca781504cbc27090be73ea1
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-10 13:53:59 -0500

    Finalize adaptive styling for nav bar

--- frontend/components/home/nav/nav_links.jsx
+++ frontend/components/home/nav/nav_links.jsx
@@ -14,12 +14,12 @@ class NavLinks extends React.Component {
   render() {
     return (
       <ul className="nav-links-container">
-        <li><Link onClick={this.handleScroll} className="nav-link" to="/home">Home</Link></li>
-        <li><Link onClick={this.handleScroll} className="nav-link brand" to="/disney">Disney</Link></li>
-        <li><Link onClick={this.handleScroll} className="nav-link brand" to="/pixar">Pixar</Link></li>
-        <li><Link onClick={this.handleScroll} className="nav-link brand" to="/marvel">Marvel</Link></li>
-        <li><Link onClick={this.handleScroll} className="nav-link brand" to="/starwars">Star Wars</Link></li>
-        <li><Link onClick={this.handleScroll} className="nav-link brand" to="/natgeo">National Geographic</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link home" to="/home">Home</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link" to="/disney">Disney</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link" to="/pixar">Pixar</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link" to="/marvel">Marvel</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link" to="/starwars">Star Wars</Link></li>
+        <li><Link onClick={this.handleScroll} className="nav-link" to="/natgeo">National Geographic</Link></li>
         <li><Link onClick={this.handleScroll} className="nav-link" to="/watchlist">Watchlist</Link></li>
       </ul>
     )
