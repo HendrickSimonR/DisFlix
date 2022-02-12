ce1683dee37140a34f3853e52a45d32a4f577f84
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-11 22:00:02 -0500

    Clean and refactor session container and components

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-// import SigninForm from './signin_form';
-// import SignupForm from './signup_form';
 
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '', password: '' };
+
+    this.state = { 
+      username: '', 
+      password: '' 
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -23,7 +26,9 @@ class SessionForm extends React.Component {
             <li key={i}>
               {error}
             </li>
+
             <br />
+
           </div>
         ))}
       </ul>
@@ -42,40 +47,52 @@ class SessionForm extends React.Component {
   }
 
   render() {
-    // console.log('session', this.props)
-
     return (
       <div className="signin-form-container">
-        <h1 className="form-header">{this.props.formGreeting}</h1>
-          <form onSubmit={this.handleSubmit} className="sign-form-box">
-            <div>
-              {this.props.errors.session.length > 0 ? this.renderErrors() : ''}
-            </div>
+        <h1 className="form-header">
+          {this.props.formGreeting}
+        </h1>
+
+        <form onSubmit={this.handleSubmit} className="sign-form-box">
+          <div>
+            {this.props.errors.session.length > 0 ? this.renderErrors() : ''}
+          </div>
             
+          <br />
+            
+          <div className="signin-form">
+            <input 
+              type="text"
+              placeholder="Username"
+              className="signin-input"
+              value={this.state.username}
+              onChange={this.update('username')}
+            />
+
             <br />
+
+            <input 
+              type="password"
+              placeholder="Password"
+              className="signin-input"
+              value={this.state.password}
+              onChange={this.update('password')}
+            />
+
+            <br />
+                
+            <input 
+              type="submit" 
+              className="session-submit" 
+              value={this.props.formType.toUpperCase()} 
+            />
+
+            <br />
+          </div>
             
-              <div className="signin-form">
-                <input type="text"
-                  value={this.state.username}
-                  onChange={this.update('username')}
-                  className="signin-input"
-                  placeholder="Username"
-                />
-              <br />
-                <input type="password"
-                  value={this.state.password}
-                  onChange={this.update('password')}
-                  className="signin-input"
-                  placeholder="Password"
-                />
-              <br />
-                <input className="session-submit" type="submit" value={this.props.formType.toUpperCase()} />
-              <br />
-            </div>
-            
-            <div className="session-option">
-              <p>{this.props.question} {this.props.link}</p>
-            </div>
+          <div className="session-option">
+            <p>{this.props.question} {this.props.link}</p>
+          </div>
         </form>
       </div>
     );
@@ -83,51 +100,3 @@ class SessionForm extends React.Component {
 }
 
 export default SessionForm;
-
-
-
-// class SessionForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = props.user;
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.update = this.update.bind(this);
-//   }
-
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     let user = Object.assign({}, this.state);
-//     this.props.submitForm(user)
-//       .then(() => this.props.history.push({pathname: '/browse'}));
-//   }
-
-//   update(field) {
-//     return e => {
-//       this.setState({
-//         [field]: e.target.value
-//       });
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {
-//           this.props.formType === 'Sign In' ? 
-//             <SigninForm
-//               handleSubmit={this.handleSubmit}
-//               update={this.update}
-//               formType={this.props.formType}
-//             /> :
-//             <SignupForm
-//               handleSubmit={this.handleSubmit}
-//               update={this.update}
-//               formType={this.props.formType}
-//             />
-//         }
-//       </div>
-//     );
-//   }
-// }
-
-// export default SessionForm;
--- frontend/components/session/signin_form_container.jsx
+++ frontend/components/session/signin_form_container.jsx
@@ -10,7 +10,7 @@ const mSTP = (state) => {
     formType: 'Sign In',
     formGreeting: 'Welcome back!',
     question: 'New to Disney Cinema?',
-    link: <Link className="question-link" to="/signup">Sign up</Link>,
+    link: <Link className="question-link" to="/signup">Sign up</Link>
   };
 };
 
@@ -22,22 +22,3 @@ const mDTP = dispatch => {
 };
 
 export default connect(mSTP, mDTP)(SessionForm);
-
-
-// const mSTP = state => {
-//   return {
-//     user: {
-//       username: '',
-//       password: ''
-//     },
-//     formType: 'Sign In'
-//   };
-// };
-
-// const mDTP = dispatch => {
-//   return {
-//     submitForm: (user) => dispatch(signin(user)),
-//   };
-// };
-
-// export default connect(mDTP, mSTP)(SessionForm);
