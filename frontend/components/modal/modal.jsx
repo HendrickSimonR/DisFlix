a14a682bd8489dcdbe4d8934fca7a34a68cb4fc0
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-11 20:14:07 -0500

    Remove unused fonts

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -12,8 +12,6 @@ function Modal({modal, closeModal}) {
   let modalComponent;
   let renderMovie = false;
   let possibleNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  // console.log('MODAL', modal, closeModal)
-  // console.log('TYPEOF', typeof(num))
   
   for (let i = 0; i < possibleNums.length; i++) {
     let possibleNum = possibleNums[i];
