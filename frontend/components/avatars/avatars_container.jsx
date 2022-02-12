539f6113ae4c310cb777fac4e33159e87403e3d1
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-11 21:22:57 -0500

    Add index and avatars container

---
+++ frontend/components/avatars/avatars_container.jsx
@@ -0,0 +1,16 @@
+// import Avatars from './avatars';
+import { connect } from "react-redux";
+import { getAvatars, getAvatar } from '../../actions/avatar_actions'; 
+
+const mSTP = state => {
+  return({
+    avatars: state.avatars,
+    user: state.session
+})};
+
+const mDTP = dispatch => ({
+  getAvatars: () => dispatch(getAvatars()),
+  getAvatar: id => dispatch(getAvatar(id))
+});
+
+// export default connect(mSTP, mDTP)(Avatars)
