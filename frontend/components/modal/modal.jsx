f60035f71858160ca5543d8078ce099c3d791b18
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-24 08:34:50 -0500

    Refactor Modal function

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -11,6 +11,7 @@ const Modal = ({ modal, closeModal }) => {
   let movieId;
   let modalComponent;
   let url = window.location.href;
+  let modalName = 'modal-background';
 
   if (modal.includes('about')) {
     about = true;
@@ -20,18 +21,19 @@ const Modal = ({ modal, closeModal }) => {
     modalComponent = <MovieModalContainer movie={movieId} />
   }
 
+  if (url.includes('watchlist')) {
+    modalName += ' watchlist';
+  } else if (url.includes('search')) {
+    modalName += ' search';
+  } else if (!url.includes('home')) {
+    modalName += ' brand';
+  } 
+
   return (
-    <div 
-      onClick={closeModal}
-      className={ url.includes('home') ? "modal-background" 
-        : url.includes('watchlist') ? "modal-background watchlist" 
-        : url.includes('search') ? "modal-background search" 
-        : "modal-background brand"} 
-    >
-      
+    <div onClick={ closeModal } className={ modalName } >
       <div 
-        className={about ? "modal-child about" : "modal-child"} 
-        onClick={e => e.stopPropagation()}
+        className={ about ? "modal-child about" : "modal-child" } 
+        onClick={ e => e.stopPropagation() }
       >
         { modalComponent }
       </div>
