ff6613c6ab16f2e9009b7c9f0202becd8ded69de
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-24 16:08:44 -0500

    Refactor watchlist button function

--- frontend/components/thumbnail/watchlist_button.jsx
+++ frontend/components/thumbnail/watchlist_button.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const WatchlistButton =  props => {
+const WatchlistButton = props => {
   const [ added, status ] = useState('add');
   
   useEffect(() => {
