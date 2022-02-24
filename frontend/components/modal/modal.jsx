633d144af8d828647b7b8a8b2f00d935d96c7223
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-24 02:19:47 -0500

    Refactor Modal function

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -4,39 +4,28 @@ import MovieModalContainer from './movie_modal_container';
 import { closeModal } from '../../actions/modal_actions';
 import { connect } from 'react-redux';
 
-const Modal = ({modal, closeModal}) => {
+const Modal = ({ modal, closeModal }) => {
   if (!modal) return null;
 
   let about;
   let movieId;
   let modalComponent;
-  let renderMovie = false;
-  let possibleNums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  
-  for (let i = 0; i < possibleNums.length; i++) {
-    let possibleNum = possibleNums[i];
-    
-    if (modal.includes(possibleNum)) {
-      renderMovie = true;
-      movieId = parseInt(modal);
-      break;
-    }
-  }
+  let url = window.location.href;
 
-  if (modal.includes('about')) about = true;
-  
-  if (renderMovie === true) {
-    modalComponent = <MovieModalContainer movie={movieId} />
-  } else {
+  if (modal.includes('about')) {
+    about = true;
     modalComponent = <AboutContainer />;
+  } else {
+    movieId = parseInt(modal);
+    modalComponent = <MovieModalContainer movie={movieId} />
   }
 
   return (
     <div 
       onClick={closeModal}
-      className={window.location.href.includes('home') ? "modal-background" 
-        : window.location.href.includes('watchlist') ? "modal-background watchlist" 
-        : window.location.href.includes('search') ? "modal-background search" 
+      className={ url.includes('home') ? "modal-background" 
+        : url.includes('watchlist') ? "modal-background watchlist" 
+        : url.includes('search') ? "modal-background search" 
         : "modal-background brand"} 
     >
       
@@ -46,7 +35,6 @@ const Modal = ({modal, closeModal}) => {
       >
         { modalComponent }
       </div>
-
     </div>
   );
 }
