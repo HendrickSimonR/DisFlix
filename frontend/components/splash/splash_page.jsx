28f7529f01ca4bf943cae589dea54d6c04008ffd
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-11-08 16:50:00 -0500

    Begins adding site protector

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -5,12 +5,70 @@ import WelcomeContainer from './welcome_container';
 class SplashPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      password: '' 
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log(this.state)
+    if (this.state.password === '$dis-project!$-94~') {
+      let splashContainer = document.getElementById('splash-container');
+      let protector = document.getElementById('site-protector');
+      let mainSplash = document.getElementById('splash-main');
+      let description = document.getElementById('splash-info');
+      protector.remove();
+      splashContainer.style.position = 'inherit';
+      mainSplash.style.visibility = 'visible';
+      description.style.visibility = 'visible';
+    }
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
 
   render() {
     return (
-      <div className="splash-container">
-        <div className="splash-main">
+      <div className="splash-container" id="splash-container">
+        <div id='site-protector'>
+          <form onSubmit={this.handleSubmit} className="sign-form-box">
+            {/* <div>
+              {this.props.errors.session.length > 0 ? this.renderErrors() : ''}
+            </div> */}
+
+            <br />
+
+            <div className="signin-form">
+              <input 
+                type="password"
+                placeholder="Password Please"
+                className="signin-input"
+                value={this.state.password}
+                onChange={this.update('password')}
+              />
+
+              <br />
+
+              <input 
+                type="submit" 
+                className="session-submit" 
+                value="Submit" 
+              />
+
+              <br />
+            </div>
+          </form>
+        </div>
+        <div className="splash-main" id='splash-main'>
           <Link to="/">
             <img src={window.logo} className="splash-logo" />
           </Link>
@@ -22,7 +80,7 @@ class SplashPage extends React.Component {
           <Route exact path="/" handleDemo={this.handleDemo} component={WelcomeContainer} />
         </div>
 
-        <div className="splash-info-container">
+        <div className="splash-info-container" id='splash-info'>
           <div className="info-container odd">
             <div className="splash-info">
               <div className="info-text-left">
